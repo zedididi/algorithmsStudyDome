2b417b8c3559cf94e3db5f23f36132b275f44224
LiuZedi <1605103328@qq.com>
2019-04-17 00:41:35 +0800

    insert_sort

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 src        
 ——sort  
 ——————bubble_sort.java      
-冒泡排序 时间复杂度 平均O(n^2) 最长O(n^2) 最好O(n)     
+冒泡排序 时间复杂度 平均O(n^2) 最长O(n^2) 最好O(n) 空间复杂度 O(1)                  
 ——————quick_sort.java           
-快速排序 时间复杂度 平均O(nlogn) 最长O(n^2)  空间复杂度 平均O(logn) 最长O(n)      
+快速排序 时间复杂度 平均O(nlogn) 最长O(n^2)  空间复杂度 平均O(logn) 最长O(n)          
+——————insert_sort.java
+插入排序 时间复杂度 平均O(n^2) 最长O(n^2) 最好O(n)  空间复杂度 O(1)           
---
+++ src/sort/insert_sort.java
@@ -0,0 +1,86 @@
+package sort;
+
+/**
+ * @program: algorithmsStudyDome
+ * @Author： LiuZedi
+ * @Date： 2019/4/16 23:55
+ */
+public class insert_sort {
+    private int[] array;
+
+    public insert_sort(int[] array){
+        this.array=array;
+    }
+
+    //被备注的是自己写的
+   /* public void sort(){
+        int temp,key;
+        for(int i=1;i<array.length;i++){
+            for (int j=i-1;j>=0;j--){
+                key=array[i];
+                if (j==i-1&&key>=array[j]){
+                   break;
+                }
+                if (j==0&&key<array[0]){
+                    move(0,i-1,array);
+                    array[j]=key;
+                    break;
+                }
+                if(key<array[j]&&key>=array[j-1]){
+                    move(j,i-1,array);
+                    array[j]=key;
+                    break;
+                }
+            }
+        }
+    }
+
+    public void move(int start,int end,int[] array){
+        for (int i=end;i>=start;i--){
+            array[i+1]=array[i];
+        }
+    }*/
+
+   //来自书本的
+   public void sort(){
+       if (array==null){
+           throw  new RuntimeException("array is null!!");
+       }
+       int length=array.length;
+       if (length>0){
+           int temp;
+           for(int i=1;i<length;i++){
+
+               temp=array[i];
+               int j=i;
+               for(;j>0&&array[j-1]>temp;j--){
+                   array[j]=array[j-1];
+               }
+               array[j]=temp;
+           }
+       }
+
+   }
+
+
+    public void printAll(){
+        int time=1;
+        for (int i:array
+        ) {
+            System.out.print(i+"   ");
+            if (time%10==0)
+                System.out.println();
+            time++;
+        }
+    }
+}
+
+class insert_sort_test{
+
+    public static void main(String[] args){
+        int[] array={47,29,27,99,43,23,80,1,34};
+        insert_sort insertSort=new insert_sort(array);
+        insertSort.sort();
+        insertSort.printAll();
+    }
+}
