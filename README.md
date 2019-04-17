d20852eb50235de054834d1e9bfc6d4c1d0013fc
LiuZedi <1605103328@qq.com>
2019-04-17 10:00:46 +0800

    select_sort

--- README.md
+++ README.md
@@ -8,7 +8,8 @@
 &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;bubble_sort.java                    
 &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;insert_sort.java            
 &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;quick_sort.java  
-
+&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;select_sort.java      
+&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;shell_sort.java       
 ####各排序算法复杂度
 -----     
 冒泡排序 时间复杂度 平均O(n^2) 最长O(n^2) 最好O(n) 空间复杂度 O(1)                  
@@ -19,7 +20,7 @@
 
 希尔排序 时间复杂度 平均O(n^1.3) 最长O(n^2) 最好O(n) 空间复杂度 O(1)    
 
-
+选择排序 时间复杂度 平均O(n^2) 最长O(n^2) 最好O(n^2) 空间复杂度O(1)     
                     
             
      
--- src/sort/select_sort.java
+++ src/sort/select_sort.java
@@ -18,9 +18,38 @@ public class select_sort {
         }
         int length=array.length;
         if (length>0){
-            for (int i=0;i<length;i++){
-//                for (int j=)
+            int index,min;
+            for (int i=0;i<length-1;i++){
+                min=array[i];
+                index=i;
+                for (int j=i;j<length;j++){
+                    if (min>array[j]){
+                        min=array[j];
+                        index=j;
+                    }
+                }
+                array[index]=array[i];
+                array[i]=min;
             }
         }
+    }
+    public void printAll(){
+        int time=1;
+        for (int i:array
+        ) {
+            System.out.print(i+"   ");
+            if (time%10==0)
+                System.out.println();
+            time++;
+        }
+    }
+
+}
+class select_sort_test{
+    public static void main(String[] args){
+        int[] array={47,29,27,99,43,23,80,1,34,23,45,73,23};
+        select_sort selectSort=new select_sort(array);
+        selectSort.sort();
+        selectSort.printAll();
     }
 }
